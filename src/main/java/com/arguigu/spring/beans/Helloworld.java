bda443d4b62919b8daea17f81bbfdc331f6be6a4
lance017 <lance017@lance017deiMac.local>
2017-04-14 15:49:30 +0800

    init
    init project

---
+++ src/main/java/com/arguigu/spring/beans/Helloworld.java
@@ -0,0 +1,17 @@
+package com.arguigu.spring.beans;
+
+/**
+ * Created by lance017 on 2017/4/14.
+ */
+public class Helloworld {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void hello() {
+        System.out.println("hello" + name);
+    }
+}
---
+++ src/main/java/com/arguigu/spring/beans/Main.java
@@ -0,0 +1,12 @@
+package com.arguigu.spring.beans;
+
+/**
+ * Created by lance017 on 2017/4/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Helloworld helloworld = new Helloworld();
+        helloworld.setName("atguigu");
+        helloworld.hello();
+    }
+}
